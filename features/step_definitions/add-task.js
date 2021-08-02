b2496465a0234343070caad4bf2cdf30e738bbdf
Jessica Blankemeier <jessicablankemeier@gmail.com>
2021-08-01 18:14:17 -0700

    created step-definitions folder w/ with test file returning 'undefined'

---
+++ features/step_definitions/add-task.js
@@ -0,0 +1,16 @@
+import {Given, When, Then } from "cucumber";
+
+Given('Max has a task list', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  When('Max adds the task {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+  Then('{string} should be in the task list', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
